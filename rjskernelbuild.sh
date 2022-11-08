04e8adf08e2c2e7cb151d58a9c6a3e870b26c12f
Ryan Shoff <ryanshoff@gmail.com>
2022-11-08 15:29:07 -0600

    initial kernel patches for INNOMAKER ov9281 sensor and faster RPI v2 cam

---
+++ rjskernelbuild.sh
@@ -0,0 +1,10 @@
+export LOCALVERSION=-test
+export TEGRA_KERNEL_OUT=/media/ubuntu/673fe9d8-6f00-4487-b046-8ccee8268a74/tx2nx/build
+export TEGRA_MODULES_OUT=/media/ubuntu/673fe9d8-6f00-4487-b046-8ccee8268a74/tx2nx/modsout/
+#make ARCH=arm64 O=$TEGRA_KERNEL_OUT tegra_defconfig
+make ARCH=arm64 O=$TEGRA_KERNEL_OUT -j4
+make ARCH=arm64 O=$TEGRA_KERNEL_OUT modules_install INSTALL_MOD_PATH=${TEGRA_MODULES_OUT}/
+sudo cp -R ${TEGRA_MODULES_OUT}/lib/modules/4.9.253-test /lib/modules
+sudo cp /media/ubuntu/673fe9d8-6f00-4487-b046-8ccee8268a74/tx2nx/build/arch/arm64/boot/dts/tegra186-p3636-0001-p3509-0000-a01.dtb /boot/test.dtb
+sudo cp $TEGRA_KERNEL_OUT/arch/arm64/boot/Image /boot/Image-4.9.253-test
+
